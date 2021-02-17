806b5fdd6de503d5f08389f08102c9dc61dc5d74
Zeshan Rasul <zeshanrasul@gmail.com>
2021-02-17 07:53:37 +0000

    Closest Hit and Miss Shaders

---
+++ Paradox/shaders/ClosestHit.hlsl
@@ -0,0 +1,14 @@
+#include "Common.hlsl"
+
+[shader("closesthit")]
+void ClosestHit(inout HitInfo payload, Attributes attrib)
+{
+	uint triangleIndex = PrimitiveIndex();
+	float3 barycentrics = float3((1.0f - attrib.uv.x - attrib.uv.y), attrib.uv.x, attrib.uv.y);
+	VertexAttributes vertex = GetVertexAttributes(triangleIndex, barycentrics);
+
+	int2 coord = floor(vertex.uv * textureResolution.x);
+	float3 colour = albedo.Load(int3(coord, 0)).rgb;
+
+	payload.ShadedColourAndHitT = float4(colour, RayTCurrent());
+}
