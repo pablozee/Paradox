7b86c0ba1721177c08dff64826cb2c515b7c4f4b
Zeshan Rasul <zeshanrasul@gmail.com>
2021-02-27 18:36:22 +0000

    Render to G Buffer Textures WIP

--- Paradox/shaders/PixelShader.hlsl
+++ Paradox/shaders/PixelShader.hlsl
@@ -1,11 +1,57 @@
+cbuffer SceneCB : register(b0)
+{
+	matrix			 view;
+	float4			 viewOriginAndTanHalfFovY;
+	float2			 resolution;
+	float			 numDirLights;
+	float			 numPointLights;
+	float3			 randomSeedVector0;
+	float			 padding;
+	float3			 randomSeedVector1;
+	float			 padding1;
+	DirectionalLight directionalLights[10];
+	PointLight		 pointLights[10];
+}
+
+cbuffer MaterialCB : register(b1)
+{
+	float3			 ambient;
+	float			 shininess;
+	float3			 diffuse;
+	float			 ior;
+	float3			 specular;
+	float			 dissolve;
+	float3			 transmittance;
+	float			 roughness;
+	float3			 emission;
+	float			 metallic;
+	float4			 textureResolution;
+	float			 sheen;
+	int				 useTex;
+}
+
 struct PSInput
 {
-	float4 PosL : SV_POSITION;
+	float3 PosW : SV_POSITION;
 	float2 TexC : TEXCOORD;
-	float3 NormalL : NORMAL;
+	float3 NormalW : NORMAL;
 };
 
-float4 main(PSInput psInput) : SV_TARGET
+struct PSOutput
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	float4 gBufferWorldPos		 : SV_Target0;
+	float4 gBufferWorldNormal	 : SV_Target1; 
+	float4 gBufferDiffuse		 : SV_Target2;
+	float4 gBufferSpecular		 : SV_Target3;
+	float4 gBufferReflectivity	 : SV_Target4; 
+};
+
+PSOutput main(PSInput psInput) : SV_TARGET
+{
+	psOutput.gBgBufferWorldPos = float4(PosW, shininess);
+	psOutput.gBgBufferNormal   = float4(NormalW, ior);
+	psOutput.gBufferDiffuse	   = float4(diffuse, 1.0f);
+	psOutput.gBufferSpecular   = float4(specular, 1.0f);
+	psOutput.gBufferReflectivity = float4(1.0f, 1.0f, 1.0f, 1.0f);
+	return psOutput;
 }
--- Paradox/shaders/VertexShader.hlsl
+++ Paradox/shaders/VertexShader.hlsl
@@ -1,23 +1,55 @@
+cbuffer SceneCB : register(b0)
+{
+	matrix			 view;
+	float4			 viewOriginAndTanHalfFovY;
+	float2			 resolution;
+	float			 numDirLights;
+	float			 numPointLights;
+	float3			 randomSeedVector0;
+	float			 padding;
+	float3			 randomSeedVector1;
+	float			 padding1;
+	DirectionalLight directionalLights[10];
+	PointLight		 pointLights[10];
+}
+
+cbuffer MaterialCB : register(b1)
+{
+	float3			 ambient;
+	float			 shininess;
+	float3			 diffuse;
+	float			 ior;
+	float3			 specular;
+	float			 dissolve;
+	float3			 transmittance;
+	float			 roughness;
+	float3			 emission;
+	float			 metallic;
+	float4			 textureResolution;
+	float			 sheen;
+	int				 useTex;
+}
+
 struct VSInput
 {
-	float3 PosL : POSITION;
+	float3 Pos : POSITION;
 	float2 TexC : TEXCOORD;
-	float3 NormalL : NORMAL;
+	float3 Normal : NORMAL;
 };
 
 struct VSOutput
 {
-	float4 PosL : SV_POSITION;
+	float3 PosW : SV_POSITION;
 	float2 TexC : TEXCOORD;
-	float3 NormalL : NORMAL;
+	float3 NormalW : NORMAL;
 };
 
 VSOutput main(VSInput vsInput)
 {
 	VSOutput vso;
-	vso.PosL = float4(vsInput.PosL, 1.0f);
+	vso.PosW = mul(vsInput.Pos, (float3x3)view);
 	vso.TexC = vsInput.TexC;
-	vso.NormalL = vsInput.NormalL;
+	vso.NormalW = mul(vsInput.Normal, (float3x3)view);
 
 	return vso;
 }
