b4babe590dda4d9084afa3d8ee80f3e7dd1e6b1b
Zeshan Rasul <zeshanrasul@gmail.com>
2021-04-24 00:40:28 +0100

    Rigidbody WIP

--- Paradox/Physics/body.h
+++ Paradox/Physics/body.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <DirectXMath.h>
+#include "../ParadoxMath.h"
 
 using namespace DirectX;
 
@@ -18,5 +18,21 @@ protected:
 	double angularDamping;
 	XMFLOAT3 position;
 	Quaternion orientation;
+	XMFLOAT3 velocity;
+	XMFLOAT3 rotation;
+	
+	// Derived Data:
+	XMFLOAT3X3 inverseInertiaTensorWorld;
+	double motion;
+	bool isAwake;
+	bool canSleep;
+	XMFLOAT4X4 transformationMatrix;
 
+	// Force and Torque Accumulators:
+	XMFLOAT3 forceAccum;
+	XMFLOAT3 torqueAccum;
+	XMFLOAT3 acceleration;
+	XMFLOAT3 lastFrameAcceleration;
+
+public:
 };
