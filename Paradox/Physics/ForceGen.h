f41ddfcaa1b7b43958cf428be605c6d58e6351d1
pablozee <zeshanrasul@gmail.com>
2021-06-30 21:27:37 +0100

    Force Generation Header File Complete

--- Paradox/Physics/ForceGen.h
+++ Paradox/Physics/ForceGen.h
@@ -204,3 +204,256 @@ public:
 
 	// virtual void updateForce(Particle *particle, double duration) = 0;
 };
+
+/**
+ * A force generator that applies an aerodynamic force.
+ */
+class Aero : public ForceGenerator
+{
+
+protected:
+
+	/**
+	 * Holds the aerodynamic tensor for the surface in body space
+	 */
+	Matrix3 tensor;
+
+	/**
+	 * Holds the relative position of the aerodynamic surface in
+	 * body coordinates.
+	 */
+	Vector3 position;
+
+	/**
+	 * Holds a pointer to a vector containing the windspeed of the
+	 * environment. This is easier than managing a separate windspeed
+	 * vector per generator and having to update it manually as the
+	 * wind changes.
+	 */
+	const Vector3 windspeed;
+
+public:
+
+	/**
+	 * Creates a new aerodynamic force generator with the given properties.
+	 */
+	Aero(const Matrix3& tensor, const Vector3& position,
+		const Vector3* windspeed);
+
+	/**
+	 * Applies the force to the given rigid body.
+	 */
+	virtual void updateForce(RigidBody* body, double duration);
+
+protected:
+
+	/**
+	 * Uses an explicit tensor matrix to update the force on the given
+	 * rigid body. This is exactly the same as for updateForce except
+	 * it takes an explicit tensor.
+	 */
+
+	virtual void updateForceFromTensor(RigidBody* body,
+									   double duration,
+									   const Matrix3* tensor);
+};
+
+/**
+ * A force generator with a control aerodynamic surface. This requires
+ * three inertia tensors, for the two extremes and 'resting' position
+ * of the control surface. The latter tensor is the one inherited from
+ * the base class, the two extremes are defined in this class.
+ */
+class AeroControl : public Aero
+{
+	
+protected:
+
+	/**
+	 * The aerodynamic tensor for the surface, when the control is at
+	 * its' maximum value.
+	 */
+	Matrix3 maxTensor;
+
+	/**
+	 * The aerodynamic tensor for the surface, when the control is at
+	 * its' minimum value.
+	 */
+	Matrix3 minTensor;
+
+	/**
+	 * The current position of the control for this surface. This should 
+	 * range between -1 (in which case the minTensor value is used),
+	 * through 0 (where the base class tensor value is used) to +1 
+	 * (where the maxTensor value is used).
+	 */
+	double controlSetting;
+
+private:
+
+	/**
+	 * Calculates the final aerodynamic tensor for the current control setting.
+	 */
+	Matrix3 getTensor();
+
+public:
+
+	/**
+	 * Creates a new aerodynamic control surface with the given properties.
+	 */
+	AeroControl(const Matrix3& base,
+				const Matrix3& min,
+				const Matrix3& max,
+				const Vector3& position,
+				const Vector3* windspeed);
+
+	/**
+	 * Sets the control position of this control. This should range
+	 * between -1 (in which case the minTensor value is used), through
+	 * 0 (where the base class tensor is used) to +1 (where the maxTensor
+	 * value is used). 
+	 * Values outside that range give undefined results.
+	 */
+	void setControl(double value);
+
+	/**
+	 * Applies the force to the given rigid body.
+	 */
+	virtual void updateForce(RigidBody* body, double duration);
+};
+
+/**
+ * A force generator with an aerodynammic surface that can be re-oriented
+ * relative to its' rigid body.
+ */
+class AngledAero : public Aero
+{
+	/**
+	 * Holds the orientation of the aerodynamic surface relative
+	 * to the rigid body to which it is attached.
+	 */
+	Quaternion orientation;
+
+public:
+
+	/**
+	 * Creates a new aerodynamic surface with the given properties.
+	 */
+	AngledAero(const Matrix3& tensor,
+			   const Vector3& position,
+			   const Vector3* windspeed);
+
+	/**
+	 * Sets the relative orientation of the aerodynamic surface,
+	 * relative to the rigid body it is attached to. Note that
+	 * this doesn't affect the point of connection of the surface
+	 * to the body.
+	 */
+	void setOrientation(const Quaternion& quat);
+
+	/**
+	 * Applies the given force to the rigid body.
+	 */
+	virtual void updateForce(RigidBody* body, double duration);
+};
+
+/**
+ * A force generator to apply a buoyant force to a rigid body
+ */
+class Buoyancy : public ForceGenerator
+{
+	/**
+	 * The maximum submersion depth of the object before it
+	 * generates its' maximum buoyancy force.
+	 */
+	double maxDepth;
+
+	/**
+	 * The volume of the object.
+	 */
+	double volume;
+
+	/**
+	 * The height of the water plane above y=0.
+	 * The plane will be parallel to the XZ plane.
+	 */
+	double waterHeight;
+
+	/**
+	 * The density of the liquid. Pure water has a density of
+	 * 1000kg per cubic meter.
+	 */
+	double liquidDensity;
+
+	/**
+	 * The centre of buoyancy of the rigid body, in body coordinates.
+	 */
+	Vector3 centreOfBuoyancy;
+
+public:
+
+	/**
+	 * Creates a new buoyancy force with the given parameters.
+	 */
+	Buoyancy(const Vector3 centreOfBuoyancy,
+			 double maxDepth,
+			 double volume,
+			 double waterHeight,
+			 double liquidDensity = 1000.0);
+
+	/**
+	 * Applies the force to the given rigid body;
+	 */
+	virtual void updateForce(RigidBody* body, double duration);
+};
+
+/**
+ * Holds all the force generators and the bodies they apply to.
+ */
+class ForceRegistry
+{
+
+protected:
+	
+	/**
+	 * Keeps track of one force generator and the body it applies to.
+	 */
+	struct ForceRegistration
+	{
+		RigidBody* body;
+		ForceGenerator* fg;
+	};
+
+	/**
+	 * Holds the list of registrations.
+	 */
+	typedef std::vector<ForceRegistration> Registry;
+	Registry registrations;
+
+public:
+
+	/**
+	 * Registers the given force generator to apply to the given body.
+	 */
+	void add(RigidBody* body, ForceGenerator* fg);
+
+	/**
+	 * Removes the given registered pair from the registry.
+	 * If the pair is not registered, this method will have
+	 * no effect.
+	 */
+	void remove(RigidBody* body, ForceGenerator* fg);
+
+	/**
+	 * Clears all registrations from the registry. This will
+	 * not delete the bodies of the force generators themselves,
+	 * just the records of their connection.
+	 */
+	void clear();
+
+	/**
+	 * Calls all the force generators to update the forces of their
+	 * corresponding bodies.
+	 */
+	void updateForces(double duration);
+};
