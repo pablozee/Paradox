360774de29b73cb420faf035f9ab6ccee329d208
pablozee <zeshanrasul@gmail.com>
2021-06-30 11:51:43 +0100

    Fine Collision Implemention Continued

--- Paradox/Physics/CollideFine.cpp
+++ Paradox/Physics/CollideFine.cpp
@@ -173,3 +173,317 @@ unsigned CollisionDetector::SphereAndHalfSpace(const CollisionSphere& sphere, co
 	return 1;
 }
 
+unsigned CollisionDetector::SphereAndSphere(const CollisionSphere& one, const CollisionSphere& two, CollisionData* data)
+{
+	// Make sure we have contacts
+	if (data->contactsLeft <= 0) return 0;
+
+	// Cache the sphere positions
+	Vector3 positionOne = one.GetAxis(3);
+	Vector3 positionTwo = two.GetAxis(3);
+
+	// Find the vector between the objects
+	Vector3 midline = positionOne - positionTwo;
+	double size = midline.magnitude();
+
+	// See if it is large enough
+	if (size <= 0.0f || size >= one.radius + two.radius)
+	{
+		return 0;
+	}
+
+	// We manually create the normal, because we have
+	// the size to hand.
+	Vector3 normal = midline * ((double)1.0 / size);
+
+	Contact* contact = data->contacts;
+	contact->contactNormal = normal;
+	contact->contactPoint = positionOne + midline * (double)0.5;
+	contact->penetration = (one.radius + two.radius - size);
+	contact->SetBodyData(one.body, two.body, data->friction, data->restitution);
+
+	data->AddContacts(1);
+	return 1;
+}
+
+/**
+ * This function checks if the two boxes overlap
+ * along the given axis, returning the amount of overlap.
+ * The final parameter toCentre is used to pass in the
+ * vector between the boxes centre points, to avoid having
+ * to recalculate it each time.
+ */
+static inline double PenetrationOnAxis(
+	const CollisionBox& one,
+	const CollisionBox& two,
+	const Vector3& axis,
+	const Vector3& toCentre
+)
+{
+	// Project the half-size of one onto axis
+	double oneProject = TransformToAxis(one, axis);
+	double twoProject = TransformToAxis(two, axis);
+
+	// Project this onto the axis
+	double distance = abs(toCentre * axis);
+
+	// Return the overlap (i.e. positive indicates
+	// overlap, negative indicates separation)
+	return oneProject + twoProject - distance;
+}
+
+static inline bool TryAxis(
+	const CollisionBox& one,
+	const CollisionBox& two,
+	Vector3 axis,
+	const Vector3& toCentre,
+	unsigned index,
+	double& smallestPenetration,
+	unsigned& smallestCase
+)
+{
+	// Make sure we have a normalized axis and don't check
+	// almost parallel axes
+	if (axis.squareMagnitude() < 0.0001) return true;
+	axis.normalise();
+
+	double penetration = PenetrationOnAxis(one, two, axis, toCentre);
+
+	if (penetration < 0) return false;
+	if (penetration < smallestPenetration)
+	{
+		smallestPenetration = penetration;
+		smallestCase = index;
+	}
+	return true;
+}
+
+void FillPointFaceBoxBox(
+	const CollisionBox& one,
+	const CollisionBox& two,
+	const Vector3& toCentre,
+	CollisionData* data,
+	unsigned best,
+	double penetration
+)
+{
+	/**
+	 * This method is called when we know that a vertex from
+	 * box two is in contact with box one
+	 */
+	Contact* contact = data->contacts;
+
+	/**
+	 * We know which axis the collision is on (i.e. best),
+	 * but we need to work out which of the two faces on
+	 * this axis.
+	 */
+	Vector3 normal = one.GetAxis(best);
+	if (one.GetAxis(best) * toCentre > 0)
+	{
+		normal = normal * -1.0f;
+	}
+
+	/**
+	 * Work out which vertex of box two we're colliding with.
+	 * Using toCentre doesn't work.
+	 */
+	Vector3 vertex = two.halfSize;
+	if (two.GetAxis(0) * normal < 0) vertex.x = -vertex.x;
+	if (two.GetAxis(1) * normal < 0) vertex.y = -vertex.y;
+	if (two.GetAxis(2) * normal < 0) vertex.z = -vertex.z;
+
+	// Create the contact data
+	contact->contactNormal = normal;
+	contact->penetration = penetration;
+	contact->contactPoint = two.GetTransform() * vertex;
+	contact->SetBodyData(one.body, two.body, data->friction, data->restitution);
+}
+
+static inline Vector3 ContactPoint(
+	const Vector3& pOne,
+	const Vector3& dOne,
+	double oneSize,
+	const Vector3& pTwo,
+	const Vector3& dTwo,
+	double twoSize,
+
+	/**
+	 * If this is true and the contact point is outside the edge
+	 * (in the case of an edge-face contact) then we use ones'
+	 * midpoint, otherwise we use twos'.
+	 */
+	bool useOne
+)
+{
+	Vector3 toSt, cOne, cTwo;
+	real dpStaOne, dpStaTwo, dpOneTwo, smOne, smTwo;
+	real denom, mua, mub;
+
+	smOne = dOne.squareMagnitude();
+	smTwo = dTwo.squareMagnitude();
+	dpOneTwo = dTwo * dOne;
+
+	toSt = pOne - pTwo;
+	dpStaOne = dOne * toSt;
+	dpStaTwo = dTwo * toSt;
+
+	denom = smOne * smTwo - dpOneTwo * dpOneTwo;
+
+	// Zero denominator indicates parrallel lines
+	if (real_abs(denom) < 0.0001f) {
+		return useOne ? pOne : pTwo;
+	}
+
+	mua = (dpOneTwo * dpStaTwo - smTwo * dpStaOne) / denom;
+	mub = (smOne * dpStaTwo - dpOneTwo * dpStaOne) / denom;
+
+	// If either of the edges has the nearest point out
+	// of bounds, then the edges aren't crossed, we have
+	// an edge-face contact. Our point is on the edge, which
+	// we know from the useOne parameter.
+	if (mua > oneSize ||
+		mua < -oneSize ||
+		mub > twoSize ||
+		mub < -twoSize)
+	{
+		return useOne ? pOne : pTwo;
+	}
+	else
+	{
+		cOne = pOne + dOne * mua;
+		cTwo = pTwo + dTwo * mub;
+
+		return cOne * 0.5 + cTwo * 0.5;
+	}
+}
+
+// This preprocessor definition is only used as a convenience 
+// in the BoxAndBox contact generation method.
+#define CHECK_OVERLAP(axis, index) \
+	if (!TryAxis(one, two, (axis), toCentre, (index), penetration, best)) return 0;
+
+unsigned CollisionDetector::BoxAndBox(const CollisionBox& one, const CollisionBox& two, CollisionData* data)
+{
+	// if (!IntersectionTests::BoxAndBox(one, two)) return 0;
+
+	// Find the vector between the two centres
+	Vector3 toCentre = two.GetAxis(3) - one.GetAxis(3);
+
+	// We start assuming there is no contact
+	double penetration = INFINITY;
+	unsigned best = 0xffffff;
+
+	// Now we check each axes, returning if it gives us
+   // a separating axis, and keeping track of the axis with
+   // the smallest penetration otherwise.
+	CHECK_OVERLAP(one.GetAxis(0), 0);
+	CHECK_OVERLAP(one.GetAxis(1), 1);
+	CHECK_OVERLAP(one.GetAxis(2), 2);
+
+	CHECK_OVERLAP(two.GetAxis(0), 3);
+	CHECK_OVERLAP(two.GetAxis(1), 4);
+	CHECK_OVERLAP(two.GetAxis(2), 5);
+
+	// Store the best axis-major, in case we run into almost
+	// parallel edge collisions later
+	unsigned bestSingleAxis = best;
+
+	CHECK_OVERLAP(one.GetAxis(0) % two.GetAxis(0), 6);
+	CHECK_OVERLAP(one.GetAxis(0) % two.GetAxis(1), 7);
+	CHECK_OVERLAP(one.GetAxis(0) % two.GetAxis(2), 8);
+	CHECK_OVERLAP(one.GetAxis(1) % two.GetAxis(0), 9);
+	CHECK_OVERLAP(one.GetAxis(1) % two.GetAxis(1), 10);
+	CHECK_OVERLAP(one.GetAxis(1) % two.GetAxis(2), 11);
+	CHECK_OVERLAP(one.GetAxis(2) % two.GetAxis(0), 12);
+	CHECK_OVERLAP(one.GetAxis(2) % two.GetAxis(1), 13);
+	CHECK_OVERLAP(one.GetAxis(2) % two.GetAxis(2), 14);
+
+	// Make sure we've got a result
+	assert(best != 0xffffff);
+
+	/**
+	 * We now know there's a collision and we know which
+	 * of the axes gave the smallest penetration. We now
+	 * can deal with it in different ways depending on the
+	 * case.
+	 */
+	if (best < 3)
+	{
+		// We've got a vertex of box two on a face of box one
+		FillPointFaceBoxBox(one, two, toCentre, data, best, penetration);
+		data->AddContacts(1);
+		return 1;
+	}
+	else if (best < 6)
+	{
+		/**
+		 * We've got a vertex of box one on a face of box two.
+		 * We use the same algorithm as above, but swap around
+		 * one and two (and therefore also the vector between
+		 * their centres).
+		 */
+		FillPointFaceBoxBox(two, one, toCentre * -1.0f, data, best - 3, penetration);
+		data->AddContacts(1);
+		return 1;
+	}
+	else
+	{
+		// We've got an edge-edge contact. Find out which axes
+		best -= 6;
+		unsigned oneAxisIndex = best / 3;
+		unsigned twoAxisIndex = best % 3;
+		Vector3 oneAxis = one.GetAxis(oneAxisIndex);
+		Vector3 twoAxis = two.GetAxis(twoAxisIndex);
+		Vector3 axis = oneAxis % twoAxis;
+		axis.normalise();
+
+		// The axis should point from box one to box two
+		if (axis * toCentre > 0) axis = axis * -1.0f;
+
+		/**
+		 * We have the axes, but not the edges: each axis has 4 edges
+		 * parallel to it, we need to find which of the 4 for each
+		 * object. We do that by finding the point in the centre of 
+		 * the edge. We know that its' component in the direction of
+		 * the box's collision axis is zero (it's a mid-point) and we 
+		 * determine which of the extremes in each of the other axes
+		 * is closest.
+		 */
+		Vector3 ptOnOneEdge = one.halfSize;
+		Vector3 ptOnTwoEdge = two.halfSize;
+
+		for (unsigned i = 0; i < 3; i++)
+		{
+			if (i == oneAxisIndex) ptOnOneEdge[i] = 0;
+			else if (one.GetAxis(i) * axis > 0) ptOnOneEdge[i] = -ptOnOneEdge[i];
+
+			if (i == twoAxisIndex) ptOnTwoEdge[i] = 0;
+			else if (two.GetAxis(i) * axis < 0) ptOnTwoEdge[i] = -ptOnTwoEdge[i];
+		}
+
+		// Move them into world coordinates (they are already oriented correctly
+		// since they have been derived from the axes).
+		ptOnOneEdge = one.transform * ptOnOneEdge;
+		ptOnTwoEdge = two.transform * ptOnTwoEdge;
+
+		// So we have a point and a direction for the colliding edges.
+		// We need to find out the point of closest approach of the 
+		// two line-segments.
+		Vector3 vertex = ContactPoint(ptOnOneEdge, oneAxis, one.halfSize[oneAxisIndex],
+									  ptOnTwoEdge, twoAxis, two.halfSize[twoAxisIndex],
+									  bestSingleAxis > 2);
+
+		// We can fill the contact
+		Contact* contact = data->contacts;
+
+		contact->penetration = penetration;
+		contact->contactNormal = axis;
+		contact->contactPoint = vertex;
+		contact->SetBodyData(one.body, two.body, data->friction, data->restitution);
+		data->AddContacts(1);
+		return 1;
+	}
+	return 0;
+}
+#undef CHECK_OVERLAP
